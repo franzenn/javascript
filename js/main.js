77a8ee74dc5f833fabc6a3b3c3f3e40db3d2f1e8
franzenn <franco_ivan_avila@hotmail.com>
2022-07-12 21:47:32 -0300

    Simulación de proyecto

--- js/main.js
+++ js/main.js
@@ -1,26 +1,21 @@
-let total = parseInt(prompt("Cuantos examenes tuviste en tu ultima materia?"));
+alert("Bienvenidos a nuestro e-commerce de El CHARRO!")
+let edad = prompt("Para ingresar primero diganos a continuación su edad: ")
 
-let numeroIngresado = parseInt(prompt("Ingresa todas tus notas y te dire tu promedio"))
-
-let contador=+numeroIngresado;
-
-for(let i=1;i<total;i++){
-    contador+=numeroIngresado;
-    numeroIngresado=parseInt(prompt("Ingresa otra nota"));
+while (edad<18){
+    alert("Disculpe, no puede ingresar siendo menor de edad.")
+    let yaEsMayor = prompt("Ya es mayor de edad? Reponda con SI/NO")
+    if(yaEsMayor=="si" || yaEsMayor=="SI"){
+        edad = prompt("Que edad tiene?")
+    }   
 }
 
-let promedio = contador/total;
-alert("Su promedio es de: "+ promedio);
+let seguridad = parseInt(prompt("Okey, para asegurarnos de que en realidad es mayor de edad diganos el año de su nacimiento..."))
 
-if(promedio<7){
-    let estudiaste=prompt("Decime la verdad, estudiaste?");
-        while(estudiaste=="si"){
-            estudiaste=prompt("No me mientas!!!")
-        }
-        if(estudiaste=="no"){
-            alert("Ponete las pilas!!!")
-        }
+if(seguridad<2004){
+    alert("Disfruta de nuestra pagina!")
 }
 else{
-    alert("Felicitaciones por aprobar la materia!")
+    while(seguridad>2004){
+        alert("Te vas de mi pagina.")
+    }
 }
