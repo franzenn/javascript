f520dbf6983b0fcfab9ecffafae2aa84798b7fd3
Franco <franco_ivan_avila@hotmail.com>
2022-07-21 23:21:54 -0300

    Modificación de toUppercase, se agregó busqueda/filtro al array y metodo foreach

--- js/main.js
+++ js/main.js
@@ -12,12 +12,12 @@ function Persona (nombre, apellido){
     const cliente = new Persona (nombre, apellido)
     
     
-    alert("Hola "+ cliente.nombre);
-    
+    alert("Hola "+ cliente.nombre.toUpperCase());
+
     
     let pedirProducto = prompt("Quiere ingresar un producto a su carrito? SI/NO");
     
-    while(pedirProducto == "SI" || pedirProducto == "si" || pedirProducto == "Si"){
+    while(pedirProducto.toUpperCase() === "SI"){
         productos.push(prompt("Ingrese producto"));
         pedirProducto = prompt("Quiere ingresar otro producto? SI/NO");
     }
@@ -26,18 +26,18 @@ function Persona (nombre, apellido){
     
     let confirmacion = prompt("Quiere agregar mas productos a su carrito?");
     
-    while(confirmacion == "SI" || confirmacion == "si"){
+    while(confirmacion.toUpperCase() == "SI"){
         productos.push(prompt("Ingrese producto"));
         confirmacion = prompt("Quiere ingresar otro producto? SI/NO");
     }
-    if(confirmacion=="no" || confirmacion=="NO"){
-        alert("Perfecto, ya termino de hacer sus compras!")
+    if(confirmacion.toUpperCase() == "NO"){
+        alert("Perfecto, ya terminó de hacer sus compras!")
     }
-    alert("Se va a llevar los siguientes productos: "+productos);
+    alert("Se va a llevar los siguientes productos: " + productos);
     
     let eliminar = prompt("Quiere eliminar algun producto? SI/NO");
     
-    while (eliminar=="si"  ||  eliminar=="SI"){
+    while (eliminar.toUpperCase() =="SI"){
         let productoAEliminar = prompt("¿Cual?");
         let index = productos.indexOf(productoAEliminar);
         if(index != -1){
@@ -47,3 +47,23 @@ function Persona (nombre, apellido){
     }
     
     alert("Su carrito contiene: "+productos); 
+
+
+
+    productos.forEach( (producto) => {   //MUESTRA POR CONSOLA LOS PRODUCTOS DEL ARRAY MEDIANTE UN FOREACH
+        console.log(producto);
+    })
+
+
+    const resultado = productos.find((el) => el =="coca cola");    //EN CASO DE AGREGAR AL ARRAY "coca cola" Y "fernet" 
+    const resultado2 = productos.find((el) => el =="fernet");      //POR ALERT SE MUESTRA UN MENSAJE OCULTO
+
+    if(resultado!=undefined && resultado2!=undefined){
+        alert("Salio fernet con coca? :D");
+    }
+
+
+    const filtro = productos.filter((el)=> el.includes('zapatillas'));  //SE FILTRAN TODOS LOS PRODUCTOS DEL ARRAY QUE CONTENGAN "zapatillas"
+    console.log(filtro);                                                //LOS MUUESTRA POR CONSOLA
+
+    
